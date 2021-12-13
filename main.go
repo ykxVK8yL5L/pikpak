47d7ba128d0f4462d603fd815f1969abcfebe33e
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-12-13 10:38:23 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("dist/*.html")
+	r.Static("/pikpak/assets", "./dist/assets/")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
